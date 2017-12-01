5ab54f91d1a1b2ef37b34434e0130d4a31277fac
Sanjana Desai <sanjana_desai@live.ca>
2017-12-01 13:59:13 -0500

    created login form

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import LoginForm from '../Containers/LoginFormContainer';
-// import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class Home extends Component { 
+
+  handleLogin = () => { 
+    this.props.history.push('/login');
+  }
+
+  handleRegister = () => { 
+    this.props.history.push('/register');
+  }
+
   render() { 
     return( 
       <div className="container-fluid">
         <h1 className="text-center">Welcome to OH.io</h1>
         <div>   
           <div className="col-6">
-            <button className="btn btn-primary" type="button" >Login</button> 
+            <button className="btn btn-primary" type="button" onClick={this.handleLogin}>Login</button> 
           </div> 
           <div className="col-6"> 
-            <button className="btn btn-primary" type="button">Register</button> 
+            <button className="btn btn-primary" type="button" onClick={this.handleRegister}>Register</button> 
           </div> 
         </div> 
       </div> 
@@ -21,4 +30,5 @@ class Home extends Component {
 }
 
 
-export default Home;
+
+export default withRouter(Home);
