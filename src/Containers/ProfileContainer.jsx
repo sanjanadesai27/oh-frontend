5a01aab5e7d940c471f86703534702bf78e875da
Susu <su.yu@mail.mcgill.ca>
2017-12-02 14:13:21 -0500

    add profile page

---
+++ src/Containers/ProfileContainer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Profile from '../Components/Profile.jsx';
+
+class UserProfileContainer extends Component { 
+  render() { 
+    return(
+      <Profile/>         
+    );
+  }
+}
+
+export default ProfileContainer;
