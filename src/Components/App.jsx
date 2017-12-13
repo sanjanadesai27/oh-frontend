46393e21b66b28ac1c9f896a842be79036a44f1e
Achooo <ashvitha297@gmail.com>
2017-12-13 09:05:38 -0500

    set up tutors

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -8,7 +8,7 @@ import {
 import Landing from './Landing.jsx';
 import Login from './LoginForm.jsx';
 import StudentRegister from './StudentRegister.jsx';
-// import UserProfile from './UserProfile.jsx';
+import TutorRegister from './TutorRegister.jsx'
 import Feed from '../Containers/FeedContainer';
 import Student from '../Containers/StudentContainer';
 import Thread from '../Containers/ThreadContainer';
@@ -63,7 +63,7 @@ class App extends Component {
     })
   }
 
-  registerHandler = (e) => { 
+  studentRegisterHandler = (e) => { 
     e.preventDefault();
     let name = document.querySelector('div.field.firstname input[name="firstname"]').value;
     let surname = document.querySelector('div.field.lastname input[name="lastname"]').value;
@@ -108,6 +108,53 @@ class App extends Component {
     }); 
   }
 
+  tutorRegisterHandler = (e) => { 
+    e.preventDefault();
+    let name = document.querySelector('div.field.firstname input[name="firstname"]').value;
+    let surname = document.querySelector('div.field.lastname input[name="lastname"]').value;
+    let username = document.querySelector('div.field.username input[name="username"]').value;
+    let email = document.querySelector('div.field.email input[name="email"]').value;
+    let password = document.querySelector('div.field.password input[name="password"]').value;
+
+    let formData = { name, surname, username, email, password}; 
+    console.log(formData);
+    fetch('/createTutor',{ 
+      headers: { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }, 
+      method: 'POST',
+      body: JSON.stringify(formData),
+    })
+    .then(res => res.json())
+    .then(res => { 
+      let token = res.token;
+      if (token) {
+
+        window.localStorage.setItem("userToken", JSON.stringify(res.token));
+        window.localStorage.setItem("id", res.user.id);
+        window.localStorage.setItem("username", res.user.username);
+
+        this.setState({
+          isLoggedIn: true,
+          userId: res.user.id
+        });
+        history.push(`/feed/${this.state.userId}`);
+        
+      } else { 
+        this.setState({ 
+          loginError:"User not created"
+        });
+        history.push('/');
+      }
+    })
+    .catch(err => { 
+      this.setState({ 
+        loginError:"User not created"
+      });
+    }); 
+  }
+
   componentDidMount() { 
     if(!this.state.isLoggedIn) { 
       this.setState({
@@ -126,7 +173,11 @@ class App extends Component {
       return (<Landing isLoggedIn={this.state.isLoggedIn} />);
     }
     const StudentRegistrationPage = (props) => { 
-      return ( <StudentRegister submitHandler={this.registerHandler} /> )
+      return ( <StudentRegister submitHandler={this.studentRegisterHandler} /> )
+    }
+
+    const TutorRegistrationPage = (props) => { 
+      return ( <TutorRegister submitHandler={this.tutorRegisterHandler} /> )
     }
 
     return(
@@ -135,7 +186,7 @@ class App extends Component {
           <Route exact path="/" render={LandingPage} />
           <Route path="/login" render={LoginPage} />
           <Route path="/studentregister" component={StudentRegistrationPage} />
-          {/* <Route path="/tutorregister" component={TutorRegistrationPage}/> */}
+          <Route path="/tutorregister" component={TutorRegistrationPage}/>
            <Route path="/feed/:id" component={Feed}/> 
 
           <Route path="/student/:id" component={Student}/>
